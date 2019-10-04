a0b2d5cc67f730e091b251e688ad78677531c892
jordan <jordansuber@gmail.com>
2019-10-04 19:30:50 -0400

    Tess Two API integrated

---
+++ app/src/main/java/MainActivity.java
@@ -0,0 +1,15 @@
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        OcrManager manager = new OcrManager();
+        manager.initAPI();
+    }
+}
---
+++ app/src/main/java/OcrManager.java
@@ -0,0 +1,23 @@
+import android.graphics.Bitmap;
+
+import com.example.cs4322.MainApplication;
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+public class OcrManager {
+
+    TessBaseAPI baseAPI = null;
+
+    public void initAPI(){
+
+        baseAPI = new TessBaseAPI();
+        // after copy, path to trained data is getExternalFilesDir(null)+"/tessdata/"+"eng.traineddata";
+        String dataPath = MainApplication.instance.getTessDataParentDirectory();
+        baseAPI.init(dataPath, "eng");
+    }
+    public String startRecognizer(Bitmap bitmap){
+        if(baseAPI == null)
+            initAPI();
+        baseAPI.setImage(bitmap);
+        return baseAPI.getUTF8Text();
+    }
+}
