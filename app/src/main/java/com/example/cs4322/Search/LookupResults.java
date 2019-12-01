761dd9b3fc935d9feb7849ad862110c3d7dc63da
Quinten <quintencallen@gmail.com>
2019-12-01 18:34:20 -0500

    Added Google Books search functionality

--- app/src/main/java/com/example/cs4322/Search/LookupResults.java
+++ app/src/main/java/com/example/cs4322/Search/LookupResults.java
@@ -1,16 +1,20 @@
 package com.example.cs4322.Search;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,12 +28,15 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import com.example.cs4322.Favorites.BookDetails;
 import com.example.cs4322.Favorites.FavoritesMenu;
 import com.example.cs4322.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -38,6 +45,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import tessTwo.OcrManager;
 
 
 public class LookupResults extends AppCompatActivity {
@@ -52,14 +60,17 @@ public class LookupResults extends AppCompatActivity {
     private DatabaseReference mRef;
     private FirebaseUser user;
 
+    private TessBaseAPI tessBaseAPI;
+
     private ArrayList<BookItem> bookList;
 
     private RequestQueue mRequestQueue;
     private static  final  String BASE_URL="https://www.googleapis.com/books/v1/volumes?q=";
 
-    Button search;
-    TextView searchTxt;
-    String userID;
+    private Button search;
+    private TextView searchTxt;
+    private FloatingActionButton camera;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,19 +104,26 @@ public class LookupResults extends AppCompatActivity {
                 String title = bookList.get(position).getTitle();
                 String author = bookList.get(position).getAuthor();
                 String isbn = bookList.get(position).getISBN();
+                String img = bookList.get(position).getThumbnail();
+                String details = bookList.get(position).getDetails();
+//
+//                if (isbn.equals("No ISBN")) {
+//                    Toast.makeText(LookupResults.this, "This book has no ISBN and can not be added to favorites", Toast.LENGTH_LONG).show();
+//                    return;
+//                }
+//
+//                mRef.child(userID).child("Books").child(isbn).child("Title").setValue(title);
+//                mRef.child(userID).child("Books").child(isbn).child("Author").setValue(author);
+//                mRef.child(userID).child("Books").child(isbn).child("ISBN").setValue(isbn);
+//
+//                Toast.makeText(LookupResults.this, "New book added to Favorites!", Toast.LENGTH_SHORT).show();
 
-                if (isbn.equals("No ISBN")) {
-                    Toast.makeText(LookupResults.this, "This book has no ISBN and can not be added to favorites", Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                mRef.child(userID).child("Books").child(isbn).child("Title").setValue(title);
-                mRef.child(userID).child("Books").child(isbn).child("Author").setValue(author);
-                mRef.child(userID).child("Books").child(isbn).child("ISBN").setValue(isbn);
-
-                Toast.makeText(LookupResults.this, "New book added to Favorites!", Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent(LookupResults.this, FavoritesMenu.class);
+                Intent intent = new Intent(LookupResults.this, BookDetails.class);
+                intent.putExtra("title", title);
+                intent.putExtra("author", author);
+                intent.putExtra("isbn", isbn);
+                intent.putExtra("img", img);
+                intent.putExtra("details", details);
                 startActivity(intent);
             }
         });
@@ -115,6 +133,8 @@ public class LookupResults extends AppCompatActivity {
 
         searchTxt = findViewById(R.id.searchTxt);
         search = findViewById(R.id.searchBtn);
+        camera = findViewById(R.id.cameraBtn);
+
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -123,6 +143,36 @@ public class LookupResults extends AppCompatActivity {
                 search();
             }
         });
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePic, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        //preview.setImageBitmap(bitmap);
+
+//        tessBaseAPI = new TessBaseAPI();
+//        tessBaseAPI.init(Environment.getExternalStorageDirectory().toString() + "/Tess", "eng");
+//        tessBaseAPI.setImage(bitmap);
+//
+//        String result = tessBaseAPI.getUTF8Text();
+//        tessBaseAPI.end();
+//
+//        searchTxt.setText(result);
+
+        OcrManager manager = new OcrManager();
+        manager.initAPI();
+        String result = manager.startRecognizer(bitmap);
+
+        searchTxt.setText(result);
     }
 
     @Override
@@ -154,7 +204,10 @@ public class LookupResults extends AppCompatActivity {
                                 JSONObject item = items.getJSONObject(i);
                                 JSONObject volumeInfo = item.getJSONObject("volumeInfo");
 
-
+                                description = "No Description";
+                                author = "Anonymous";
+                                title = "Untitled";
+                                isbn ="No ISBN";
 
                                 try{
                                     title = volumeInfo.getString("title");
@@ -193,8 +246,8 @@ public class LookupResults extends AppCompatActivity {
                                 String previewLink = volumeInfo.getString("previewLink");
                                 String url = volumeInfo.getString("infoLink");
 
-
-                                bookList.add(new BookItem(title, isbn, author));
+                                if (!isbn.equals("No ISBN"))
+                                    bookList.add(new BookItem(title, isbn, author, thumbnail, description));
 
                                 resultsView.setAdapter(mAdapter);
                             }
@@ -244,7 +297,7 @@ public class LookupResults extends AppCompatActivity {
             return;
         }
         String final_query=search_query.replace(" ","+");
-        Uri uri=Uri.parse(BASE_URL+final_query+"&maxResults=40");
+        Uri uri=Uri.parse(BASE_URL+final_query+"&printType=books&filter=ebooks&maxResults=40");
         Uri.Builder builder = uri.buildUpon();
 
         parseJson(builder.toString());
